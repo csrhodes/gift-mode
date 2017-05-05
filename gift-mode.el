4114608e08f06a271dfba7e1ac454c3d35f4ea5f
Christophe Rhodes <csr21@cantab.net>
2017-05-05 08:21:03 +0100

    simple font-locking
    (where by "simple" I mean "oh now I remember how all this works")

--- gift-mode.el
+++ gift-mode.el
@@ -8,11 +8,24 @@
     (modify-syntax-entry ?\n "> b" table)
     table))
 
+(defface gift-keyword '((t (:inherit font-lock-keyword-face)))
+  "keywords for GIFT quizzes")
+
+(defface gift-category '((t (:inherit outline-1)))
+  "category for GIFT quizzes")
+
+(defface gift-question-name '((t (:inherit outline-2)))
+  "question name in GIFT quizzes")
+
+(defvar gift-font-lock-keywords
+  '(("\\(\\$CATEGORY\\):\s-*\\(\\$course\\$/?\\|\\)\\(.*?\\)\\(//\\|$\\)" (1 'gift-keyword) (2 'gift-keyword) (3 'gift-category))
+    ("::\\([^:]\\|\\\\:\\)+::" . 'outline-2)))
+
 (define-derived-mode gift-mode text-mode "GIFT"
   "Major mode for editing GIFT format quizzes.
 \\{gift-mode-map}"
   :syntax-table gift-mode-syntax-table
-  (setq font-lock-defaults (list nil nil))
+  (setq font-lock-defaults (list '(gift-font-lock-keywords) nil))
   (set (make-local-variable 'outline-regexp) "\\(\\$CATEGORY\\|::\\)")
   (set (make-local-variable 'outline-heading-end-regexp)
        "\\(\\$CATEGORY.*\n\\|::[^:]+::\\(.\\|\n\\)\\)")
