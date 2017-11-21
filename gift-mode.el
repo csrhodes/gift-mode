f8c9a495e3c6a47dbfdcb719bcbd0f8522297340
Christophe Rhodes <c.rhodes@gold.ac.uk>
2017-11-21 14:53:52 +0000

    commands for modifying credit in multiple-answer questions
    also C-<right> and C-<left> bindings for them in gift-mode-map

--- gift-mode.el
+++ gift-mode.el
@@ -88,6 +88,47 @@
     ("\\(\\$CATEGORY\\):\s-*\\(\\$course\\$/?\\|\\)\\(.*?\\)\\(//\\|$\\)" (1 'gift-keyword) (2 'gift-keyword) (3 'gift-category))
     ("::\\([^:]\\|\\\\:\\)+::" . 'gift-question-name)))
 
+(defvar gift-credit '("-100" "-50" "-33.3333" "-25" "-20" "-16.6667" "16.6667" "20" "25" "33.3333" "50" "100"))
+
+(defun gift-decrease-credit ()
+  (interactive)
+  (save-excursion
+    (save-match-data
+      (let ((bol (save-excursion (beginning-of-line) (point)))
+	    (eol (save-excursion (end-of-line) (point))))
+	(if (search-forward-regexp "~%\\(-?[0-9.]+\\)%" eol t)
+	    (let* ((current (match-string 1))
+		   (cpos (position current gift-credit :test 'equal)))
+	      (when (and cpos (> cpos 0))
+		(replace-match (elt gift-credit (1- cpos)) t t nil 1)))
+	  (beginning-of-line)
+	  (when (search-forward-regexp "~%\\(-?[0-9.]+\\)%" eol t)
+	    (let* ((current (match-string 1))
+		   (cpos (position current gift-credit :test 'equal)))
+	      (when (and cpos (> cpos 0))
+		(replace-match (elt gift-credit (1- cpos)) t t nil 1)))))))))
+
+(defun gift-increase-credit ()
+  (interactive)
+  (save-excursion
+    (save-match-data
+      (let ((bol (save-excursion (beginning-of-line) (point)))
+	    (eol (save-excursion (end-of-line) (point))))
+	(if (search-forward-regexp "~%\\(-?[0-9.]+\\)%" eol t)
+	    (let* ((current (match-string 1))
+		   (cpos (position current gift-credit :test 'equal)))
+	      (when (and cpos (< cpos (1- (length gift-credit))))
+		(replace-match (elt gift-credit (1+ cpos)) t t nil 1)))
+	  (beginning-of-line)
+	  (when (search-forward-regexp "~%\\(-?[0-9.]+\\)%" eol t)
+	    (let* ((current (match-string 1))
+		   (cpos (position current gift-credit :test 'equal)))
+	      (when (and cpos (< cpos (1- (length gift-credit))))
+		(replace-match (elt gift-credit (1+ cpos)) t t nil 1)))))))))
+
+(define-key gift-mode-map (kbd "<C-left>") 'gift-decrease-credit)
+(define-key gift-mode-map (kbd "<C-right>") 'gift-increase-credit)
+
 ;;;###autoload
 (define-derived-mode gift-mode text-mode "GIFT"
   "Major mode for editing GIFT format quizzes.
